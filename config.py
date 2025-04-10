55dd91a90348e49d2e13b52e6916c244a2acccc3
akaahmadaka <akaahmad662@gmail.com>
2025-04-10 15:56:20 +0500

    fix

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
 
 # TODO: Move ADMIN_IDS to environment variable (e.g., os.environ.get('ADMIN_IDS', '').split(','))
 # Ensure the environment variable is a comma-separated list of integers.
-ADMIN_IDS = os.environ.get('ADMIN_IDS', "")
+admin_ids_str = os.environ.get('ADMIN_IDS', '')
+ADMIN_IDS = [int(admin_id.strip()) for admin_id in admin_ids_str.split(',') if admin_id.strip().isdigit()]
 DEEP_LINK_TEMPLATE = "t.me/{bot_username}?startgroup=startloop"
 WELCOME_MSG = "👋 Add me in group and send /startloop\nOr click here 👉 {deep_link}"
 GLOBAL_DELAY = 30  # in seconds
